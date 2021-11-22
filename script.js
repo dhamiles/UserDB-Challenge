6f6363d78084e2ce48f0e9fd00afac1214330911
Dominic Harris <dommiles-ha@hotmail.com>
2021-11-22 17:32:03 +0000

    Second Commit
    Adds all functionality except the interval db updating

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ ws = new WebSocket("ws://localhost:8082");
 
 // All code the for the log-in/registration process
 
-// Assign Element Variables
+// Assign Element Variables from the html
 const signInBtn = document.querySelector(".signin--btn");
 const registerBtn = document.querySelector(".register--btn");
 const signOutBtn = document.querySelector(".signout--btn");
 const uidInput = document.querySelector(".input--uid");
-const newUidInput = document.querySelector(".input--newuid")
+const newUidInput = document.querySelector(".input--newuid");
 const userContent = document.querySelector(".user--content");
 const uidHeading = document.querySelector(".uid--heading");
 const dataDisplay = document.querySelector(".database");
-
+const dataAdd = document.querySelector(".dataadd--btn");
+const dataInput = document.querySelector(".input--data");
+const dataDelete = document.querySelector(".datadel--btn");
+const dataDelInput = document.querySelector(".input--datadel");
 
 // Assign General State variables
 let signedIn = false;
@@ -22,97 +25,125 @@ let currentData = null;
 
 // Assign all functionality when sign-in button pressed
 signInBtn.addEventListener("click", function () {
-
-    // If no user signed in, give the button function
-    if (!signedIn) {
-        // set the message to send as an object with event and data keys
-        let message = {
-            event: "SIGNIN-ATTEMPT",
-            data: uidInput.value
-        }
-        ws.send(JSON.stringify(message));
-    }  else {
-        uidInput.value = '';
-    }
-})
+  // If no user signed in, give the button function
+  if (!signedIn) {
+    // set the message to send as an object with event and data keys
+    let message = {
+      event: "SIGNIN-ATTEMPT",
+      data: uidInput.value,
+    };
+    ws.send(JSON.stringify(message));
+  } else {
+    uidInput.value = "";
+  }
+});
 
 // Assign all functionality when sign-out button pressed
 signOutBtn.addEventListener("click", function () {
-    
-    // Only function is currently signed-in
-    if (signedIn) {
+  // Only function is currently signed-in
+  if (signedIn) {
+    // when signing out, send the current user data back to the server and update
+    let message = {
+      event: "USER-SIGNOUT",
+      data: currentData,
+    };
 
-        // when signing out, send the current user data back to the server and update
-        let message = {
-            event: "USER-SIGNOUT",
-            data: currentData
-        }
+    //stringify and send the data to the server
+    ws.send(JSON.stringify(message));
 
-        //stringify and send the data to the server
-        ws.send(JSON.stringify(message));
-
-        // change all variables to reset the page
-        currentUser = null;
-        currentData = null;
-        signedIn = false;
-        userContent.classList.add("hidden")
-    } 
-})
+    // change all variables to reset the page
+    currentUser = null;
+    currentData = null;
+    signedIn = false;
+    dataInput.value = "";
+    userContent.classList.add("hidden");
+  }
+});
 
 // Functionality when the register button is cliccked
 registerBtn.addEventListener("click", function () {
-    // Assign new uid input box value  to a variable for clarity
-    let newUid = newUidInput.value; 
-    // Only function if no user currently signed in
-    if (!signedIn && newUid) {
+  // Assign new uid input box value  to a variable for clarity
+  let newUid = newUidInput.value;
+  // Only function if no user currently signed in
+  if (!signedIn && newUid) {
+    // let the message to send to server be the following
+    let message = {
+      event: "NEW-USER",
+      data: newUid,
+    };
+    ws.send(JSON.stringify(message));
+  }
+  // clear the input box
+  newUidInput.value = "";
+});
 
-        // let the message to send to server be the following
-        let message = {
-            event: "NEW-USER",
-            data: newUid
-        }
-        ws.send(JSON.stringify(message));
+// Functionality on the add data button to local db
+dataAdd.addEventListener("click", function () {
+  // Only run if signed in and the datainput field not empty
+  if (signedIn && dataInput.value) {
+    currentData.push(dataInput.value);
+    dataDisplay.textContent = currentData;
+  }
+  // clear the input field when button pressed
+  dataInput.value = "";
+});
+
+// Add all the functionality for deleting data from local db
+dataDelete.addEventListener("click", function () {
+  // if signedin and data is present in the delete input box
+  if (signedIn && dataDelInput.value) {
+    // verify data exists in the current user local db
+    if (currentData.includes(dataDelInput.value)) {
+      let i = currentData.indexOf(dataDelInput.value);
+      // Delete the array at the index of the data point to be deleted
+      currentData.splice(i, 1);
+      dataDisplay.textContent = currentData;
+    } else {
+      alert(
+        "Data you typed cannot be found\nEither entry does not exist or data was not typed exactly"
+      );
     }
-    // clear the input box
-    newUidInput.value = '';
-})
+  }
+  dataDelInput.value = "";
+});
 
 // Create an event listener that listens for messages from the server
-ws.addEventListener("message", message => {
-    // parse the JSON string into new variable m
-    let m = JSON.parse(message.data);
-    // log the object for debugging purposes!
-    console.log(m);
-    
-    if (m.event === "SIGNIN-REPLY") {
-        // if m.data false, failed sign in attempt
-        if (!m.data) {
-            // clear the uid input and alert user
-            uidInput.value = '';
-            alert('Invalid UID\nPlease enter correct UID or register');
-        } else { // else the UID is valid, set the current data and user variables
-            uidInput.value = '';
-            currentUser = m.data.uid;
-            currentData = m.data.data;
-            // set signedIn to true
-            signedIn = true;
-            // unhide the main content
-            userContent.classList.remove("hidden");
-            // add the uid to the heading
-            uidHeading.textContent = `User ID: ${m.data.uid}`;
-            // Display the current user data
-            dataDisplay.textContent = currentData;
+ws.addEventListener("message", (message) => {
+  // parse the JSON string into new variable m
+  let m = JSON.parse(message.data);
+  // log the object for debugging purposes!
+  console.log(m);
 
-            // log current user data for debugging
-            console.log(currentUser,currentData,signedIn);
-        }
+  if (m.event === "SIGNIN-REPLY") {
+    // if m.data false, failed sign in attempt
+    if (!m.data) {
+      // clear the uid input and alert user
+      uidInput.value = "";
+      alert("Invalid UID\nPlease enter correct UID or register");
+    } else {
+      // else the UID is valid, set the current data and user variables
+      uidInput.value = "";
+      currentUser = m.data.uid;
+      currentData = m.data.data;
+      // set signedIn to true
+      signedIn = true;
+      // unhide the main content
+      userContent.classList.remove("hidden");
+      // add the uid to the heading
+      uidHeading.textContent = `User ID: ${m.data.uid}`;
+      // Display the current user data
+      dataDisplay.textContent = currentData;
 
-    // If the event is  a newuser-response     
-    } else if (m.event === "NEWUSER-REPLY") {
-        if (m.data) {
-            alert('Account successfully created\nSign in with your new UID');
-        } else {
-            alert('Either UID is invalid or already in use,  try a different UID');
-        }
+      // log current user data for debugging
+      console.log(currentUser, currentData, signedIn);
     }
-})
+
+    // If the event is  a newuser-response
+  } else if (m.event === "NEWUSER-REPLY") {
+    if (m.data) {
+      alert("Account successfully created\nSign in with your new UID");
+    } else {
+      alert("Either UID is invalid or already in use,  try a different UID");
+    }
+  }
+});
--- server/server.js
+++ server/server.js
@@ -8,91 +8,114 @@ let currentData;
 
 // Create a simple array object to serve as a database to store user data
 let database = [
-//    {
-//        uid: '12345',
-//        data: [1,2,3]
-//   }
+  // format of the user objects that get added to the database
+  //    {
+  //        uid: '12345',
+  //        data: [1,2,3]
+  //   }
 ];
 
-wss.on("connection", ws => {
+/*
+Below are all the messages that can be recieved from the client
+*/
 
-    ws.on("message", message => {
-        m = JSON.parse(message);
-        // log the message for debugging purposes
-        console.log(m);
+wss.on("connection", (ws) => {
+  // group and deal with all the possible messages
+  ws.on("message", (message) => {
+    m = JSON.parse(message);
+    // log the message for debugging purposes
+    console.log(m);
 
-        // If the event is a sign-in attempt
-        if (m.event === "SIGNIN-ATTEMPT") {
-            // run the validate UID on the data in the message
-            let validate = validateUID(m.data);
-            //update current data and user serverside if valid UID
-            if (validate) {
-                currentUser = validate.uid;
-                currentData = validate.data;
-            }
-            // create the reply object
-            let reply = {
-                event: "SIGNIN-REPLY",
-                data: validate
-            }
-            
-            ws.send(JSON.stringify(reply));
+    // If the event is a sign-in attempt
+    if (m.event === "SIGNIN-ATTEMPT") {
+      // run the validate UID on the data in the message
+      let validate = validateUID(m.data);
+      //update current data and user serverside if valid UID
+      if (validate) {
+        currentUser = validate.uid;
+        currentData = validate.data;
+      }
+      // create the reply object
+      let reply = {
+        event: "SIGNIN-REPLY",
+        data: validate,
+      };
 
-        // If the event is a user signing out
-        } else if (m.event === "USER-SIGNOUT") {
-            // Update the current data variable 
-            currentData = m.data;
-            // Clear current user and data variables
-            currentData = null;
-            currentUser = null;
-            // log current user and data for debugging
-            console.log(currentUser,currentData);
+      ws.send(JSON.stringify(reply));
 
-        // If the event is a new  user creation
-        } else if (m.event === "NEW-USER") {
+      // If the event is a user signing out
+    } else if (m.event === "USER-SIGNOUT") {
+      // Call the update database function
+      updateUserDB(currentUser, m.data, currentData);
+      // Clear current user and data variables
+      currentData = null;
+      currentUser = null;
+      // log current user and data for debugging
+      console.log(currentUser, currentData);
 
-            // Call the new user function and create reply object
-            let reply = {
-                event: "NEWUSER-REPLY",
-                data:  createUser(m.data)
-            }
-            // Send the response back to the client
-            ws.send(JSON.stringify(reply))
+      // If the event is a new  user creation
+    } else if (m.event === "NEW-USER") {
+      // Call the new user function and create reply object
+      let reply = {
+        event: "NEWUSER-REPLY",
+        data: createUser(m.data),
+      };
+      // Send the response back to the client
+      ws.send(JSON.stringify(reply));
 
-            // log current user  database for debugging
-            console.log(database);
+      // log current user  database for debugging
+      console.log(database);
+    }
+  });
+});
 
-        }
-    })
-})
+/*
+Below are all functions/methods on the server side
+*/
 
 // Create a function to validate the UIDs from the database object
 const validateUID = function (uid) {
-    let r = false;
-    for (var i=0; i < database.length; i++) {
-        if (database[i].uid === uid) {
-            r = database[i];
-        } else {continue}
+  let r = false;
+  for (var i = 0; i < database.length; i++) {
+    if (database[i].uid === uid) {
+      r = database[i];
+    } else {
+      continue;
+    }
     // if not in the db return false
     return r;
+  }
+};
+
+// Function that updates the data when user logs out to be called above
+const updateUserDB = function (uid, messagedata, olddata) {
+  // Compare the old and new data, if they are the same change nothing
+  if (JSON.stringify(messagedata) == JSON.stringify(olddata)) {
+    return null;
+  } else {
+    // find current user in db and update their data
+    for (const u of database) {
+      if (u.uid === uid) {
+        u.data = messagedata;
+        break;
+      }
     }
-}
+  }
+};
 
-// Create function that updates the data when user logs out to be called above
-
-// Create a function that creates a new user in the database 
+// Create a function that creates a new user in the database
 const createUser = function (uid) {
-    //loop through all the users in the database
-    for (var i=0; i < database.length; i++) {
-        if (database[i].uid === uid) {
-            return false;
-        } // If not in the db then create new user object
-    }
-    // If not in the db then create new user object
-    let userObj = {
-        uid: uid,
-        data: []
-    } // push to database
-    database.push(userObj);
-    return true;
-}
+  //loop through all the users in the database
+  for (var i = 0; i < database.length; i++) {
+    if (database[i].uid === uid) {
+      return false;
+    } // If not in the db then create new user object
+  }
+  // If not in the db then create new user object
+  let userObj = {
+    uid: uid,
+    data: [],
+  }; // push to database
+  database.push(userObj);
+  return true;
+};
